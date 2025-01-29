c4c876e253dff31f0e7a5a014b56fc5b57f2e79a
sabbirHossain <sabbirshnt@gmail.com>
2025-01-29 15:51:00 +0600

    done

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setShowRightSidebar } from "../../../redux/features/stateSlice";
 
 const Navbar = () => {
+  const { balance, username } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   return (
@@ -194,7 +195,7 @@ const Navbar = () => {
             >
               <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
             </svg>
-            <span className>6607</span>
+            <span className>{username}</span>
           </div>
 
           <div className="flex items-center justify-between p-2 gap-2 border-gray/20 rounded-lg w-auto h-8 lg:border">
@@ -218,7 +219,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2">
             <div className="flex flex-wrap items-center flex-grow gap-1 text-sm">
               <span className="ml-auto text-yellow">
-                <span>₹4.00</span>
+                <span>₹{balance}</span>
               </span>
             </div>
             <svg
