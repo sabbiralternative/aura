17a98f25bb3c3110e2d7084ad9d90114435c05b5
sabbirHossain <sabbirshnt@gmail.com>
2025-08-30 18:33:03 +0600

    done

--- src/components/shared/events/Counter.jsx
+++ src/components/shared/events/Counter.jsx
@@ -33,31 +33,35 @@ const Counter = ({ firstEvent }) => {
   }, [timer, timerDuration, circumference, initialValue]);
 
   return (
-    <div className="absolute z-50 top-[30%] left-1/2 -translate-x-1/2">
-      <div className="w-[60px] lg:scale-150 origin-center aspect-square rounded-full flex justify-center items-center relative">
-        <div className="border-[6px] h-[83%] w-[83%] rounded-full aspect-square absolute border-white/10 bg-black/10" />
+    <>
+      {timer > 0 && (
+        <div className="absolute z-50 top-[30%] left-1/2 -translate-x-1/2">
+          <div className="w-[60px] lg:scale-150 origin-center aspect-square rounded-full flex justify-center items-center relative">
+            <div className="border-[6px] h-[83%] w-[83%] rounded-full aspect-square absolute border-white/10 bg-black/10" />
 
-        <svg
-          className="absolute top-0 left-0 w-full h-full rounded-full -rotate-90"
-          style={{ stroke: strokeColor }}
-        >
-          <circle
-            cx={30}
-            cy={30}
-            r={radius}
-            fill="transparent"
-            strokeLinecap="butt"
-            stroke={strokeColor}
-            strokeWidth={6}
-            strokeDasharray={circumference}
-            strokeDashoffset={strokeDashoffset}
-          />
-        </svg>
-        <span className="text-xl drop-shadow-md font-mono font-bold text-white">
-          {timer}
-        </span>
-      </div>
-    </div>
+            <svg
+              className="absolute top-0 left-0 w-full h-full rounded-full -rotate-90"
+              style={{ stroke: strokeColor }}
+            >
+              <circle
+                cx={30}
+                cy={30}
+                r={radius}
+                fill="transparent"
+                strokeLinecap="butt"
+                stroke={strokeColor}
+                strokeWidth={6}
+                strokeDasharray={circumference}
+                strokeDashoffset={strokeDashoffset}
+              />
+            </svg>
+            <span className="text-xl drop-shadow-md font-mono font-bold text-white">
+              {timer}
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
